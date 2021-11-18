de6ae77ecbd4bf53e0e07b99d2d61021b4a94522
PedroMonteiro7 <pedromonteirogt@gmail.com>
2021-11-18 11:24:18 -0300

    Login implementado

--- login/acoesLogin.php
+++ login/acoesLogin.php
@@ -7,16 +7,18 @@ require("../database/conexao.php");
 ####   FUNÇÕES DE LOGIN/LOGOUT   ####
 
 function realizarLogin($usuario, $senha, $conexao) {
-
+    
     $sql = "SELECT * FROM tbl_administrador WHERE usuario = '$usuario'";
 
     $resultado = mysqli_query($conexao, $sql);
 
     $dadosUsuario = mysqli_fetch_array($resultado);
 
+    echo password_hash($senha, PASSWORD_DEFAULT);
+
     if (isset($dadosUsuario["usuario"]) && isset($dadosUsuario["senha"])  && password_verify($senha, $dadosUsuario["senha"])) {
 
-        $_SESSION["usuarioId"] = $dadosUsuario["id"];
+        $_SESSION["usuarioId"] = $dadosUsuario["idAdministrador"];
         $_SESSION["nome"] = $dadosUsuario["nome"];
 
         header("location: ../listagem/index.php");
@@ -35,6 +37,8 @@ switch ($_POST["acao"]) {
         $senha = $_POST["senha"];
         
         realizarLogin($usuario, $senha, $conexao);
+
+        // header("location: ../listagem/index.php");
 
         break;
 
